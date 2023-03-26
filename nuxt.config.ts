52bd30e081938c4b48c772cc63091fab8e7427b6
Igor Santos <igorasantos@gmail.com>
2023-03-26 19:05:51 -0300

    feat: add nuxt samples
    nuxt pages & style samples

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   typescript: {
     shim: false
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/_colors.scss" as *;'
+        }
+      }
+    }
   }
 })
