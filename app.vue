4fbc30f9196232000fbbf180068d1b011d34bbb6
Igor Santos <igorasantos@gmail.com>
2023-03-27 00:31:10 -0300

    feat: add common menu

--- app.vue
+++ app.vue
@@ -1,17 +1,7 @@
 <template>
-  <div>
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/top/nested/hash1">Hash1 Dynamic</NuxtLink></li>          
-          <li><NuxtLink to="/top/nested/132112312">Hash number Dynamic</NuxtLink></li>          
-        </ul>
-      </nav>
-    </header>
-    <NuxtPage />
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
 <script lang="ts" setup>
 useHead({
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/top/nested/hash1">Hash1 Dynamic</NuxtLink></li>
+          <li><NuxtLink to="/top/nested/132112312">Hash number Dynamic</NuxtLink></li>
+          <li><NuxtLink to="/top/nested/12345678912345678901">Hash 20 numbers Dynamic</NuxtLink></li>
+          <li><NuxtLink to="/top/nested/asd123sdd213213sdfse">Hash 20 chars Dynamic</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
