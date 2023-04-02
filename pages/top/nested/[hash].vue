329f038cd6e7b08b59c73f4be3f3a7d015c31194
Igor Santos <igorasantos@gmail.com>
2023-04-01 23:18:00 -0300

    feat: add local mocked data

--- pages/top/nested/[hash].vue
+++ pages/top/nested/[hash].vue
@@ -7,6 +7,13 @@
     <button @click="add">
       Add
     </button>
+    <h2 v-if="!fetchedData">Data loading</h2>
+    <div v-else>
+      <h2>Data fetched</h2>
+      <ul>
+        <li v-for="(value, key) in fetchedData" :key="key">{{key}}: {{ value }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -16,6 +23,12 @@ import type { Ref } from 'vue';
 
 interface IStateObj {
   count: number
+}
+
+interface IDataResponse {
+  firstProp: number,
+  secondProp: string,
+  dataName: string
 }
 
 // Route Validation
@@ -35,6 +48,8 @@ useHead({
 const route = useRoute();
 const stateObj: IStateObj = reactive({count: 0})
 const dataTest: Ref<string> = ref('local state')
+// const { pending, data: backendData } = useLazyFetch<IDataResponse>(`baseUrl/${route.params.hash}`)
+const { default: fetchedData } : { default: IDataResponse } = await import(`../../../api/mockedData/${route.params.hash}.json`);
 
 function add() {
   stateObj.count++;
